b411ddfab26010575e6bba9dd08a049336abc92d
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-01-09 12:35:01 +0000

    require models in data mapper setup

--- lib/data_mapper_setup.rb
+++ lib/data_mapper_setup.rb
@@ -1,5 +1,6 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require './lib/models/message'
 
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/whatsapp_conversations_#{ENV['RACK_ENV']}")
