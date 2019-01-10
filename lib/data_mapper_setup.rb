9164a7e6ab7f5f44f897546b2290fd58702879b8
Hugo Sykes <hugo@denmeadfarm.co.uk>
2019-01-10 23:43:40 +0000

    Upgrades version of json and removes seed data

--- lib/data_mapper_setup.rb
+++ lib/data_mapper_setup.rb
@@ -2,6 +2,6 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require './lib/models/message'
 
-DataMapper::Logger.new($stdout, :debug)
+DataMapper::Logger.new($stdout, :debug) unless ENV['RACK_ENV'] == 'test'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/whatsapp_conversations_#{ENV['RACK_ENV']}")
 DataMapper.finalize
